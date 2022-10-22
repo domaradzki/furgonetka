43c93c662f164fa66667fc4f904db6071be9e2fd
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2022-10-22 12:15:42 +0200

    added first variables with colors, added layout template, imported logo, and set a header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,14 @@
+import Menu from '../menu/menu';
+
 function App() {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <Menu />
+        <main className="main"></main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/menu/menu.js
@@ -0,0 +1,13 @@
+import { ReactComponent as Logo } from '../../assets/images/logo.svg';
+
+function Menu() {
+  return (
+    <header className="menu">
+      <div className="menu__content">
+        <Logo />
+      </div>
+    </header>
+  );
+}
+
+export default Menu;
